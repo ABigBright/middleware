83456c443bb137bfa9e5810e9ee594d2626d7fcd
Andrew Walker <awalker@ixsystems.com>
2024-03-26 07:05:59 -0700

    Fix

--- src/middlewared/middlewared/plugins/smb_/utils.py
+++ src/middlewared/middlewared/plugins/smb_/utils.py
@@ -1,11 +1,10 @@
 
 
-def smb_strip_comments(self, auxparam_in):
+def smb_strip_comments(auxparam_in):
         parsed_config = ""
         for entry in auxparam_in.splitlines():
             if entry == "" or entry.startswith(('#', ';')):
                 continue
             parsed_config += entry if len(parsed_config) == 0 else f'\n{entry}'
 
-        return parsed_entry
-
+        return parsed_config
